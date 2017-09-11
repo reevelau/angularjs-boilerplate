856ac7e12a412c4d04aac71f16101ad164f1aa34
Reeve <reeve@Reeves-MacBook-Pro.local>
2017-09-12 00:22:36 +0800

    able to drag the menu along the x direction

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,6 +86,7 @@ module.exports = function (grunt) {
         },
         files: [
           '<%= yeoman.app %>/{,*/}*.html',
+          '<%= yeoman.app %>/directives/{,*/}*.html',
           '.tmp/styles/{,*/}*.css',
           '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
         ]
--- app/scripts/directives/navigationdrawer.js
+++ app/scripts/directives/navigationdrawer.js
@@ -11,7 +11,48 @@ angular.module('navdrawerApp')
     return {
       templateUrl: 'views/directives/navigationDrawer.html',
       restrict: 'E',
+      /*
+      controller : ['$scope', function($scope){
+        $scope.onClick = function(){
+          console.log('click');
+        };
+
+        $scope.onHammer = function onHammer(event){
+          console.log(event);
+        }
+      }]
+      */
+      
       link: function postLink(scope, element, attrs) {
+        console.log('postLink');
+        console.log(element);
+        scope.lastPan = {};
+        scope.lastPan.endX = 0;
+        scope.lastPan.endY = 0;
+
+        scope.onHammer = function onHammer (event) {
+          console.log(`x: ${event.deltaX}, y: ${event.center.y}, isFinal: ${event.isFinal}`, event);
+          var draggable = event.element[0];
+          var translatex = scope.lastPan.endX + event.deltaX;
+          console.log(`translatex: ${translatex} = ${scope.lastPan.endX} + ${event.deltaX}`);
+          draggable.style.transform = `translate3d( ${translatex}px, 0, 0)`;
+        };
+
+        scope.onPanStart = function onPanStart(event){
+          console.log('pan start!');
+        };
+
+        scope.onPanEnd = function onPanEnd(event){
+          var bounds = element[0].getBoundingClientRect();
+          scope.lastPan.endX  = scope.lastPan.endX + event.deltaX;
+          console.log(`scope.lastPan.endX ${scope.lastPan.endX}`);
+        };
+         
+        scope.onClick = function onClick(){
+          console.log('click');
+
+        }
       }
+      
     };
   });
