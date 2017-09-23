ddd8a4ec3523ace77f8dc491a462905cf06c823e
Reeve <reeve@Reeves-MacBook-Pro.local>
2017-09-23 17:08:18 +0800

    adding a handle bar

--- app/scripts/directives/navigationdrawer.js
+++ app/scripts/directives/navigationdrawer.js
@@ -26,16 +26,32 @@ angular.module('navdrawerApp')
       link: function postLink(scope, element, attrs) {
         console.log('postLink');
         console.log(element);
+        var draggableMenu = element[0].querySelector('.touch-menu');
         scope.lastPan = {};
         scope.lastPan.endX = 0;
         scope.lastPan.endY = 0;
 
+        scope.menu = {};
+        scope.menu.draggable = draggableMenu;
+        scope.menu.width = 280;
+        scope.menu.moveTo = function (translatex){
+          var toX = scope.menu.normalizeMoveRange(translatex); 
+
+          scope.menu.draggable.style.transform = `translate3d( ${toX}px, 0, 0)`;
+        };
+        scope.menu.normalizeMoveRange = function (x){
+          if(x < -scope.menu.width)
+            return -scope.menu.width;
+          
+          if(x > 0)
+            return 0;
+
+          return x;
+        };
+
         scope.onHammer = function onHammer (event) {
-          console.log(`x: ${event.deltaX}, y: ${event.center.y}, isFinal: ${event.isFinal}`, event);
-          var draggable = event.element[0];
           var translatex = scope.lastPan.endX + event.deltaX;
-          console.log(`translatex: ${translatex} = ${scope.lastPan.endX} + ${event.deltaX}`);
-          draggable.style.transform = `translate3d( ${translatex}px, 0, 0)`;
+          scope.menu.moveTo(translatex);
         };
 
         scope.onPanStart = function onPanStart(event){
@@ -43,9 +59,22 @@ angular.module('navdrawerApp')
         };
 
         scope.onPanEnd = function onPanEnd(event){
-          var bounds = element[0].getBoundingClientRect();
-          scope.lastPan.endX  = scope.lastPan.endX + event.deltaX;
-          console.log(`scope.lastPan.endX ${scope.lastPan.endX}`);
+          //var bounds = element[0].getBoundingClientRect();
+          //scope.lastPan.endX  = scope.lastPan.endX + event.deltaX;
+          //console.log(`scope.lastPan.endX ${scope.lastPan.endX}`);
+          var xEnd = scope.menu.normalizeMoveRange(event.deltaX);
+          if(xEnd < -scope.menu.width /3  )
+          {
+            // go to -scope.menu.width
+            scope.menu.moveTo(-scope.menu.width);
+            scope.lastPan.endX = -scope.menu.width;
+          }
+          else
+          {
+            // go to 0
+            scope.menu.moveTo(0);
+            scope.lastPan.endX = 0;
+          }
         };
          
         scope.onClick = function onClick(){
